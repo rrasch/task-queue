463bb5f8e3e76ec426474de1384d2d80391c8bff
Rasan Rasch <rasan@nyu.edu>
2015-03-23 13:24:45 -0400

    Use Servolux::Server to manage number of workers and print out errors.

--- workers.rb
+++ workers.rb
@@ -34,7 +34,6 @@ module JobProcessor
   def before_executing
     puts "before_executing"
     mqhost = 'localhost'
-    foo = fuck
     begin
       puts "Connecting to #{mqhost}"
       @conn = Bunny.new(:host => mqhost, :automatically_recover => true)
@@ -101,29 +100,121 @@ def classify(str)
   str.split(/[_-]/).collect(&:capitalize).join
 end
 
-# Create our preforking worker pool. Each worker will run the code found in
-# the JobProcessor module. We set a timeout of 10 minutes. The child process
-# must send a "heartbeat" message to the parent within this timeout period;
-# otherwise, the parent will halt the child process.
-#
-# Our execute code in the JobProcessor takes this into account. It will wakeup
-# every 2 minutes, if no jobs are reserved from the RabbitMQ queue, and send
-# the heartbeat message.
-#
-# This also means that if any job processed by a worker takes longer than 10
-# minutes to run, that child worker will be killed.
-pool = Servolux::Prefork.new(:timeout => 600, :module => JobProcessor)
 
-# Start up 7 child processes to handle jobs
-pool.start 1
-puts "Waiting"
+class TaskQueueServer < ::Servolux::Server
 
-# When SIGINT is received, kill all child process and then reap the child PIDs
-# from the proc table.
-trap('INT') {
-  pool.signal 'KILL'
-  pool.reap
-}
-Process.waitall
+  # Create a preforking server that has the given minimum and maximum boundaries
+  #
+  def initialize( min_workers = 2, max_workers = 10 )
+    @logger = ::Logger.new( $stderr )
+    super( self.class.name, :interval => 2, :logger => @logger )
+    # Create our preforking worker pool. Each worker will run the
+    # code found in the JobProcessor module. We set a timeout of 10
+    # minutes. The child process must send a "heartbeat" message to
+    # the parent within this timeout period; otherwise, the parent
+    # will halt the child process.
+    #
+    # Our execute code in the JobProcessor takes this into account.
+    # It will wakeup every 2 minutes, if no jobs are reserved from
+    # the RabbitMQ queue, and send the heartbeat message.
+    #
+    # This also means that if any job processed by a worker takes
+    # longer than 10 minutes to run, that child worker will be
+    # killed.
+    @pool = Servolux::Prefork.new( :module => JobProcessor, :timeout => 600,
+                                   :min_workers => min_workers, :max_workers => max_workers )
+  end
+
+  def log( msg )
+    logger.info msg
+  end
+
+  def log_pool_status
+    log "Pool status : #{@pool.worker_counts.inspect} living pids #{live_worker_pids.join(' ')}"
+  end
+
+  def live_worker_pids
+    pids = []
+    @pool.each_worker { |w| pids << w.pid if w.alive? }
+    return pids
+  end
+
+  def shutdown_workers
+    log "Shutting down all workers"
+    @pool.stop
+    loop do
+      log_pool_status
+      break if @pool.live_worker_count <= 0
+      sleep 0.25
+    end
+  end
+
+  def log_worker_status( worker )
+    if not worker.alive? then
+      worker.wait
+      if worker.error then
+        log "Worker #{worker.pid} child error: #{worker.error.inspect}"
+      elsif worker.exited? then
+        log "Worker #{worker.pid} exited with status #{worker.exitstatus}"
+      elsif worker.signaled? then
+        log "Worker #{worker.pid} signaled by #{worker.termsig}"
+      elsif worker.stopped? then
+        log "Worker #{worker.pid} stopped by #{worker.stopsig}"
+      else
+        log "I have no clue #{worker.inspect}"
+      end
+    end
+  end
+
+  #############################################################################
+  # Implementations of parts of the Servolux::Server API
+  #############################################################################
+
+  # this is run once before the Server's run loop
+  def before_starting
+    log "Starting up the Pool"
+    # Start up child processes to handle jobs
+    @pool.start( @pool.min_workers )
+    log "Send a USR1 to add a worker                        (kill -usr1 #{Process.pid})"
+    log "Send a USR2 to kill all the workers                (kill -usr2 #{Process.pid})"
+    log "Send a INT (Ctrl-C) or TERM to shutdown the server (kill -term #{Process.pid})"
+  end
+
+  # Add a worker to the pool when USR1 is received
+  def usr1
+    log "Adding a worker"
+    @pool.add_workers
+  end
+
+  # kill all the current workers with a usr2, the run loop will respawn up to
+  # the min_worker count
+  #
+  def usr2
+    shutdown_workers
+  end
+
+  # By default, Servolux::Server will capture the TERM signal and call its
+  # +shutdown+ method. After that +shutdown+ method is called it will call
+  # +after_shutdown+ we're going to hook into that so that all the workers get
+  # cleanly shutdown before the parent process exits
+  def after_stopping
+    shutdown_workers
+  end
+
+  # This is the method that is executed during the run loop
+  #
+  def run
+    log_pool_status
+    @pool.each_worker do |worker|
+      log_worker_status( worker )
+    end
+    @pool.ensure_worker_pool_size
+  end
+end
+
+if $0 == __FILE__ then
+  tqs = TaskQueueServer.new
+  tqs.startup
+end
 
 # vim: et:

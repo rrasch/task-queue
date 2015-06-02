5e08d687622608e37f1a99a61621f03bc7c44d88
Rasan Rasch <rasan@nyu.edu>
2015-06-02 11:43:33 -0400

    Add deploy script.

---
+++ deploy.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'sshkit'
+require 'sshkit/dsl'
+
+hosts = %w{rasan@test}
+
+repo = 'https://github.com/rrasch/task-queue.git'
+
+install_dir = '/usr/local/dlib/task-queue'
+
+on hosts do |host|
+  if test "[ -d #{install_dir} ]"
+    within install_dir do
+      execute :git, :pull
+    end
+  else
+    execute :git, :clone, repo, install_dir
+  end
+  within install_dir do
+    execute './workers-ctrl', 'restart'
+  end
+end
+

e5f23cb0a463794f75b936d36a8a8f57b976467f
Rasan Rasch <rasan@nyu.edu>
2016-01-29 00:38:38 -0500

    Create library for db operations.

---
+++ lib/tasklog.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+
+require 'mysql2'
+
+class TaskLog
+
+  def initialize(config_file, logger)
+    @client = Mysql2::Client.new(
+      :default_file  => config_file,
+      :read_timeout => 30,
+      :write_timeout => 60,
+      :connect_timeout => 10,
+      :reconnect => true,
+    )
+    @logger = logger
+  end
+
+  def collection(provider, collection, coll_type, create=true)
+    collection_id = nil
+
+    select_col = @client.prepare(
+     "SELECT collection_id FROM collection
+      WHERE provider = ? AND collection = ?")
+
+    insert_col = @client.prepare(
+     "INSERT INTO collection
+      (collection_id, provider, collection, type)
+      VALUES
+      (0, ?, ?, ?)")
+
+    results = select_col.execute(provider, collection)
+    if results.count == 0
+      coll_type = find_coll_type(rstar_dir, wip_id)
+      insert_col.execute(provider, collection, coll_type)
+      collection_id = @client.last_id
+    elsif create
+      collection_id = results.first['collection_id']
+    end
+
+    return collection_id
+  end
+
+
+  def update(task)
+    select_log = @client.prepare(
+     "SELECT t.collection_id FROM task_queue_log t, collection c
+      WHERE t.collection_id = c.collection_id
+      AND t.collection_id = ? and t.wip_id = ?")
+
+    insert_log = @client.prepare(
+     "INSERT INTO task_queue_log
+      (collection_id, wip_id, state, user_id, worker_host, started, completed)
+      VALUES 
+      (?, ?, ?, ?, ?, ?, ?)")
+    
+    update_log = @client.prepare(
+     "UPDATE task_queue_log
+      SET state = ?, user_id = ?, worker_host = ?, started = ?, completed = ?
+      WHERE collection_id = ? AND wip_id = ?")
+   
+    results = select_log.execute(collection_id, wip_id)
+    if results.count == 0
+      @logger.debug "Inserting into log for #{wip_id}"
+      insert_log.execute(
+        collection_id, wip_id,
+        task['state'], task['user_id'], task['worker_host'],
+        task['started'], task['completed'])
+    else
+      @logger.debug "Updating log for #{wip_id}"
+      update_log.execute(
+        task['state'], task['user_id'], task['worker_host'],
+        task['started'], task['completed'],
+        collection_id, wip_id)
+    end
+  end
+
+  def close
+    @client.close
+  end
+
+end
--- log-job-status.rb
+++ log-job-status.rb
@@ -5,6 +5,7 @@ require 'bunny'
 require 'json'
 require 'mysql2'
 require 'optparse'
+require './lib/tasklog'
 
 # Set default options
 options = {
@@ -75,41 +76,6 @@ $stderr = logfile
 logger.debug "Lowering priority of process #{Process.pid}"
 Process.setpriority(Process::PRIO_PROCESS, 0, 19)
 
-client = Mysql2::Client.new(
-  :default_file  => options[:my_cnf],
-  :read_timeout => 30,
-  :write_timeout => 60,
-  :connect_timeout => 10,
-  :reconnect => true,
-)
-
-# XXX: create a library for db statemets
-insert_col = client.prepare(
- "INSERT INTO collection
-  (collection_id, provider, collection, type)
-  VALUES
-  (0, ?, ?, ?)")
-
-select_col = client.prepare(
- "SELECT collection_id FROM collection
-  WHERE provider = ? AND collection = ?")
-
-insert_log = client.prepare(
- "INSERT INTO task_queue_log
-  (collection_id, wip_id, state, user_id, worker_host, started, completed)
-  VALUES 
-  (?, ?, ?, ?, ?, ?, ?)")
-
-update_log = client.prepare(
- "UPDATE task_queue_log
-  SET state = ?, user_id = ?, worker_host = ?, started = ?, completed = ?
-  WHERE collection_id = ? AND wip_id = ?")
-
-select_log = client.prepare(
- "SELECT t.completed FROM task_queue_log t, collection c
-  WHERE t.collection_id = c.collection_id
-  AND t.collection_id = ? and t.wip_id = ?")
-
 conn = Bunny.new(:host   => options[:mqhost],
                  :logger => logger)
 conn.start
@@ -122,7 +88,8 @@ q.bind(x, :routing_key => "task_queue.*")
 consumer = nil
 
 begin
-  q.subscribe(:block => true, :manual_ack => true) do |delivery_info, properties, payload|
+  q.subscribe(:block => true,
+              :manual_ack => true) do |delivery_info, properties, payload|
     begin
       consumer = delivery_info[:consumer]
 
@@ -143,33 +110,14 @@ begin
       wip_id =  task['identifiers'][0]
       logger.debug "wip_id: #{wip_id}"
 
-      user_id = task['user_id'] || 'unknown'
+      coll_type = find_coll_type(rstar_dir, wip_id)
 
-      # Get collection id. Add it to table if it doesn't exist
-      results = select_col.execute(provider, collection)
-      if results.count == 0
-        coll_type = find_coll_type(rstar_dir, wip_id)
-        insert_col.execute(provider, collection, coll_type)
-        collection_id = client.last_id
-      else
-        collection_id = results.first['collection_id']
-      end
+      tasklog = TaskLog.new(options[:my_cnf])
+      collection_id = TaskLog.collection(provider, collection, coll_type)
       logger.debug "collection_id: #{collection_id}"
+      TaskLog.update(collection_id, wip_id, task)
+      TaskLog.close
 
-      results = select_log.execute(collection_id, wip_id)
-      if results.count == 0
-        logger.debug "Inserting into log for #{wip_id}"
-        insert_log.execute(
-          collection_id, wip_id,
-          task['state'], user_id, task['worker_host'],
-          task['started'], task['completed'])
-      else
-        logger.debug "Updating log for #{wip_id}"
-        update_log.execute(
-          task['state'], user_id, task['worker_host'],
-          task['started'], task['completed'],
-          collection_id, wip_id)
-      end
       ch.ack(delivery_info.delivery_tag)
     rescue Exception => e
       logger.error e

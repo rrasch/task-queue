a988c88f61edd6b4e3e1e329f6810c08104994ec
Rasan Rasch <rasan@nyu.edu>
2016-04-25 21:27:44 -0400

    Add support for input and output dirs

--- add-mb-job.pl
+++ add-mb-job.pl
@@ -11,43 +11,31 @@ use DBI;
 use Getopt::Std;
 use JSON;
 use Net::AMQP::RabbitMQ;
-use Term::ANSIColor;
-use Term::ReadKey;
 
 # Command line options
 # -h:  help message
 # -v:  verbose
-# -f:  force adding jobs to the queue
-# -b:  batch mode
-# -d:  add derivative creation job
-# -p:  add pdf creation job
-# -s:  add stitch pages job
-# -a:  add job combining 3 jobs above
-# -t:  add video transcoding job
 # -m:  hostname for messaging server
 # -r:  rstar directory
+# -i:  input directory
+# -o:  output directory
 # -c:  mysql config file
-# -i:  message priority
-# -o:  operation
+# -p:  message priority
+# -s:  service
 # -e:  extra command line args
 # -j:  json config to pass to job
 
 my %opt;
-getopts('hvfbdpsatm:r:c:i:o:e:j:', \%opt);
+getopt('h:v:mriocpsej', \%opt);
 
 if ($opt{h}) {
 	usage();
 	exit(0);
 }
 
-my $rstar_dir = $opt{r} || $ENV{RSTAR_DIR};
-if (!$rstar_dir) {
-	usage("You must specify rstar directory.");
-	exit(1);
-} elsif (! -d $rstar_dir) {
-	print STDERR "Directory $rstar_dir does not exist.\n";
-	exit(1);
-}
+my $rstar_dir  = $opt{r};
+my $input_dir  = $opt{i};
+my $output_dir = $opt{o};
 
 if ((exists($opt{i}) && !defined($opt{i}))
 	|| (defined($opt{i}) && !($opt{i} =~ /^\d+$/ && $opt{i} <= 10)))
@@ -58,7 +46,7 @@ if ((exists($opt{i}) && !defined($opt{i}))
 
 my $priority = $opt{i} || 0;
 
-my $host = $opt{m} || $ENV{MQHOST} || "localhost";
+my $host = $opt{m} || "localhost";
 
 my $my_cnf = $opt{c} || "/content/prod/rstar/etc/my-taskqueue.cnf";
 
@@ -66,77 +54,14 @@ my $extra_args = $opt{e} || '';
 
 my $json_config = $opt{j} || '';
 
-# Automatically go into batch mode if stdin isn't connected
-# to tty. Useful if using script in conjunction with xargs
-my $batch_mode = $opt{b} || !-t STDIN;
-
-my $num_flags = count_flags(@opt{'d', 'p', 's', 'a', 't', 'o'});
-
-if (!$num_flags) {
-	usage("You must set one of -d, -p, -s, -a, -t, or -o");
-	exit(1);
-} elsif ($num_flags > 1) {
-	usage("Please select only one of -d, -p, -s, -a, -t, or -o");
+if (!$opt{s}) {
+	usage("You must set -s to define the service.");
 	exit(1);
 }
 
-my ($class, $op);
-if ($opt{d}) {
-	$op = "create-derivatives";
-} elsif ($opt{p}) {
-	$op = "create-pdf";
-} elsif ($opt{s}) {
-	$op = "stitch-pages";
-} elsif ($opt{a}) {
-	$op = "gen-all";
-} elsif ($opt{t}) {
-	$op = "transcode_wip";
-} elsif ($opt{o}) {
-	($class, $op) = split(/:/, $opt{o});
-}
-
-my $wip_dir = "$rstar_dir/wip/se";
-
-my @ids = @ARGV ? @ARGV : get_dir_contents($wip_dir);
+my ($class, $op) = split(/:/, $opt{s});
 
 my $queue_name = "task_queue";
-
-my $fh;
-
-if ($batch_mode) {
-	$fh = *STDERR;
-} else {
-	my $more = "more";
-	$more .= " -R" if $^O =~ /darwin/;
-	open($fh, "|$more") or die("Can't start '$more': $!");
-}
-
-print $fh "Sending ", colored($op, 'red'), " job to ",
-  colored($host, 'red'), " for books: \n";
-for my $id (@ids)
-{
-	print $fh "$id\n";
-}
-
-unless ($batch_mode)
-{
-	close($fh);
-
-	my $answer = "";
-	do
-	{
-		print STDERR "Would you like to continue? (y)es/(n)o\n";
-		$answer = ReadLine(0);
-		$answer =~ s/^\s+//;
-		$answer =~ s/\s+$//;
-	} while $answer !~ /^(y(es)?|no?)$/i;
-
-	if ($answer =~ /^n/)
-	{
-		print STDERR "Exiting.\n\n";
-		exit(0);
-	}
-}
 
 my $mq = Net::AMQP::RabbitMQ->new();
 
@@ -163,32 +88,23 @@ $mq->queue_declare(
 	{'x-max-priority' => 10}
 );
 
-$class = $opt{t} ? "video" : "book-publisher" if !$class;
+my $login = getpwuid($<);
 
 my $dbh = DBI->connect("DBI:mysql:;mysql_read_default_file=$my_cnf");
 
-my ($provider, $collection) = $rstar_dir =~ /.*\/([^\/]+)\/([^\/]+)\/*$/;
+$dbh->do("INSERT into batch (user_id) VALUES ('$login')")
+	or die $dbh->errstr;
 
-my $sth = $dbh->prepare(qq{
-	SELECT collection_id FROM collection
-	WHERE provider = '$provider' and collection = '$collection'
-}) or die $dbh->errstr;
-$sth->execute;
-my ($collection_id) = $sth->fetchrow_array;
-
-$sth = $dbh->prepare(qq{
-	SELECT state
-	FROM task_queue_log
-	WHERE collection_id = ? and wip_id = ? 
-}) or die $dbh->errstr;
-
-my $login = getpwuid($<);
+my $batch_id = $dbh->{mysql_insertid};
 
 my $task = {
 	class       => $class,
 	operation   => $op,
 	rstar_dir   => $rstar_dir,
+	input_dir   => $input_dir,
+	output_dir  => $output_dir,
 	user_id     => $login,
+	batch_id    => $batch_id,
 	state       => 'pending',
 };
 
@@ -206,37 +122,32 @@ if ($json_config)
 	}
 }
 
-for my $id (@ids)
+if ($rstar_dir)
 {
-	my ($state);
-
-	if ($collection_id)
+	# Create a job for each wip id
+	my @ids = @ARGV ? @ARGV : get_dir_contents("$rstar_dir/wip/se");
+	for my $id (@ids)
 	{
-		$sth->execute($collection_id, $id);
-		($state) = $sth->fetchrow_array;
+		$task->{identifiers} = [$id];
+		publish($mq, $task, $priority);
 	}
+}
+else
+{
+	publish($mq, $task, $priority);
+}
 
-	if ($state && !$opt{f})
-	{
-		print STDERR "$id is already in $state state. Skipping.\n";
-		next;
-	}
 
-	$task->{identifiers} = [$id];
-
+sub publish
+{
+	my ($mq, $task, $priority) = @_;
 	my $body = $json->encode($task);
-
 	print STDERR "Sending $body\n" if $opt{v};
-
 	$mq->publish(1, "$queue_name.pending", $body,
 		{exchange => 'tq_logging'});
-
 	$mq->publish(1, $queue_name, $body, {},
 		{priority => $priority});
 }
-
-$sth->finish;
-$dbh->disconnect;
 
 
 sub usage
@@ -245,38 +156,20 @@ sub usage
 	print STDERR "\n";
 	print STDERR "$msg\n\n" if $msg;
 	print STDERR "Usage: $0 -r <rstar dir> [-m <mq host>]\n",
-		"           [-i <priority>] [-c <mysql config>]\n",
-		"           [-b] [-f]  -d | -p | -s | -a | -t | -o <operation>\n",
-		"           [-e <extra_args>] [-j <json config]\n",
+		"           [-p <priority>] [-c <mysql config>]\n",
+		"           -s <service>\n",
+		"           [-e <extra_args>] [-j <json config>]\n",
 		"           [wip_id] ...\n\n",
 		"        -m     <RabbitMQ host>\n",
 		"        -r     <R* directory>\n",
 		"        -h     flag to print help message\n",
 		"        -v     verbose output\n",
-		"        -b     batch mode, won't prompt user\n",
-		"        -f     force adding jobs to queue\n",
 		"        -i     <message priority>\n",
 		"        -c     <path to mysql config file>\n",
-		"        -o     <operation>\n",
-		"        -d     flag to create job to generate derivatives\n",
-		"        -p     flag to create job to generate pdfs\n",
-		"        -s     flag to create job to stitch pages\n",
-		"        -a     flag to create job combining 3 jobs above\n",
-		"        -t     flag to create job to transcode videos\n",
+		"        -s     <service>\n",
 		"        -e     <extra command ling args>\n",
 		"        -j     <json config file to pass to job>\n";
 	print STDERR "\n";
-}
-
-
-sub count_flags
-{
-	my $cnt = 0;
-	for my $flag (@_)
-	{
-		$cnt++ if $flag;
-	}
-	return $cnt;
 }
 
 

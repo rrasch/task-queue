eee638202244bd027939b5b7eebfd7a3539b5929
Rasan Rasch <rasan@nyu.edu>
2022-07-20 11:28:18 -0400

    Add prepend sudo to commands

--- doc/create-users.sh
+++ doc/create-users.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-getent group rstar >/dev/null || groupadd -g 450 -r rstar
+getent group rstar >/dev/null || sudo groupadd -g 450 -r rstar
 getent passwd rstar >/dev/null || \
   sudo useradd -r -m -u 450 -g rstar -s /sbin/nologin -c "rstar" rstar
 
-getent group deploy >/dev/null || groupadd -g 451 -r deploy
+getent group deploy >/dev/null || sudo groupadd -g 451 -r deploy
 getent passwd deploy >/dev/null || \
   sudo useradd -r -m -u 451 -g deploy -s /sbin/nologin -c "deploy" deploy
 
-getent group dlib >/dev/null || groupadd dlib
+getent group dlib >/dev/null || sudo groupadd dlib
 

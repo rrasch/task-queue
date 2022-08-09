e792f5290472cf0e66c11dfcd074ed02e80887ee
Rasan Rasch <rasan@nyu.edu>
2022-08-09 09:03:08 -0400

    Fix shutdown script

--- shutdown.sh
+++ shutdown.sh
@@ -2,8 +2,16 @@
 
 set -eu
 
+HOST=$(hostname -s)
+
+# temp directory
+RSTAR_TMPDIR="/content/prod/rstar/tmp"
+
 # shutdown service if this file exists
-SHUTDOWN_FILE="/content/prod/rstar/tmp/tq-shutdown.txt"
+SHUTDOWN_TRIGGER_FILE="$RSTAR_TMPDIR/tq-shutdown.txt"
+
+# store date/time of shutdown here
+SHUTDOWN_DATE_FILE="$RSTAR_TMPDIR/tq-shutdown-date-$HOST.txt"
 
 # task queue config file
 TQ_CONFIG_FILE="/content/prod/rstar/etc/task-queue.sysconfig"
@@ -34,11 +42,11 @@ get_admin_email
 [ -n "$MQHOST" ] || exit
 [ -n "$admin_email" ] || exit
 
-if [ -f $SHUTDOWN_FILE ]; then
+if [ -f $SHUTDOWN_TRIGGER_FILE -a ! -f $SHUTDOWN_DATE_FILE ]; then
 	get_msg_count
 	# make sure message queue is empty before shutting down
 	if [ "$msg_count" = "0"  ]; then
-		#rm -f $SHUTDOWN_FILE
+		date "+%Y-%m-%d %H:%M:%S" > $SHUTDOWN_DATE_FILE
 		notice="Shutting down task queue on $(hostname)"
 		echo "$notice" | systemd-cat -t task-queue
 		echo "$notice" | mail -s "$notice" $admin_email

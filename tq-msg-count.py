6981a3745a0131ecf981451ed3dc128abd1b72d2
Rasan Rasch <rasan@nyu.edu>
2023-10-05 12:20:14 -0400

    Default mqhost to localhost if MQHOST not set

--- tq-msg-count.py
+++ tq-msg-count.py
@@ -4,27 +4,34 @@ import os.path
 import pika
 import subprocess
 
-conf_file = "/content/prod/rstar/etc/task-queue.sysconfig"
 
-if os.path.exists(conf_file):
-    process = subprocess.run(
-        f"source {conf_file}; echo $MQHOST",
-        stdout=subprocess.PIPE,
-        shell=True,
-        universal_newlines=True,
-        check=True
+def main():
+    conf_file = "/content/prod/rstar/etc/task-queue.sysconfig"
+
+    if os.path.exists(conf_file):
+        process = subprocess.run(
+            f"source {conf_file}; echo $MQHOST",
+            stdout=subprocess.PIPE,
+            shell=True,
+            text=True,
+            check=True,
+        )
+        mqhost = process.stdout.strip()
+        if not mqhost:
+            mqhost = "localhost"
+    else:
+        mqhost = "localhost"
+
+    pika_conn_params = pika.ConnectionParameters(host=mqhost)
+    connection = pika.BlockingConnection(pika_conn_params)
+    channel = connection.channel()
+
+    queue = channel.queue_declare(
+        queue="task_queue", durable=True, arguments={"x-max-priority": 10}
     )
-    mqhost = process.stdout.strip()
-else:
-    mqhost = "localhost"
 
-pika_conn_params = pika.ConnectionParameters(host=mqhost)
-connection = pika.BlockingConnection(pika_conn_params)
-channel = connection.channel()
+    print(queue.method.message_count)
 
-queue = channel.queue_declare(
-    queue="task_queue", durable=True, arguments={"x-max-priority": 10}
-)
 
-print(queue.method.message_count)
-
+if __name__ == "__main__":
+    main()

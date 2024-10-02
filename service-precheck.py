e5fba839387e62a06756e5d01ce6d6dba9a8e396
Rasan Rasch <rasan@nyu.edu>
2024-10-02 19:23:39 -0400

    Check if nfs mount is operational

--- service-precheck.py
+++ service-precheck.py
@@ -63,7 +63,7 @@ def can_list_files(mountpoint, timeout=5):
             universal_newlines=True,
             timeout=timeout,
         )
-    except (sp.CalledProcessError, sp.TimeoutExpired) as e:
+    except (sp.CalledProcessError, sp.TimeoutExpired):
         return False
     else:
         return True
@@ -88,6 +88,35 @@ def check_nfs_mount(mount_point):
     return True
 
 
+def parse_fstab(file_path="/etc/fstab"):
+    fstab_entries = {}
+
+    with open(file_path, "r") as f:
+        for line in f:
+            # Ignore comments and empty lines
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+
+            # Split the line into columns
+            fields = line.split()
+            if len(fields) < 6:
+                # If we don't have all 6 fields, it's not valid
+                continue
+
+            entry = {
+                "device": fields[0],
+                "mount_point": fields[1],
+                "fs_type": fields[2],
+                "options": fields[3],
+                "dump": fields[4],
+                "pass": fields[5],
+            }
+            fstab_entries[entry["mount_point"]] = entry
+
+    return fstab_entries
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Test ports required by task queue"
@@ -97,6 +126,11 @@ def main():
     )
     parser.add_argument(
         "--smtp", action="store_true", help="Test connection to mail server"
+    )
+    parser.add_argument(
+        "--nfs",
+        action="store_true",
+        help="Test if nfs rstar mount point is accessible",
     )
     args = parser.parse_args()
 
@@ -119,6 +153,12 @@ def main():
             executor.submit(test_port, host, port): f"{host}:{port}"
             for host, port in sock_addrs
         }
+        nfs_future = None
+        if args.nfs:
+            mount_point = tqcommon.get_rstar_dir()
+            fstab = parse_fstab()
+            if mount_point in fstab and fstab[mount_point]["fs_type"] == "nfs":
+                nfs_future = executor.submit(check_nfs_mount, mount_point)
 
     success = True
     for future in futures:
@@ -126,6 +166,12 @@ def main():
         status = "open" if is_port_open else "closed"
         success = success and is_port_open
         log(f"Port {futures[future]} is {status}")
+
+    if nfs_future:
+        is_nfs_up = nfs_future.result()
+        success = success and is_nfs_up
+        status = "up" if is_nfs_up else "down"
+        log(f"Mount point {mount_point} is {status}")
 
     if success:
         status = "succeeded"

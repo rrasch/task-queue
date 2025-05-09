cc33d384551df7549c1d8e6e38e51ed49e68af95
Rasan Rasch <rasan@nyu.edu>
2025-05-09 12:49:29 -0400

    Add bag validation

--- lib/bagit.rb
+++ lib/bagit.rb
@@ -1,19 +1,26 @@
+# frozen_string_literal: true
+
 require 'open3'
+require_relative './cmd'
 
+# Class for validating bagit directories
 class Bagit
- 
-  attr_accessor :bag_dir
-  attr_accessor :bagit_tool
- 
-  def initialize(bag_dir = ".")
-    @bag_dir = bag_dir
-    @bagit_tool = `which bag`.strip
-  end
- 
-  def validate?
-    system @bagit_tool, 'verifyvalid', @bag_dir
-    $?.success?
+  BIN_DIR = '/content/prod/rstar/bin'
+
+  BAGIT_CMD = "#{BIN_DIR}/bagit/bag"
+
+  def initialize(args)
+    @args = args.clone
+    @logger = @args['logger']
+    @cmd = Cmd.new(args)
   end
 
+  def validate
+    unless @args['input_path']
+      err_msg = 'Bagit.validate: Must specify input_path.'
+      @logger.error(err_msg)
+      return { success: false, output: err_msg }
+    end
+    @cmd.do_cmd("#{BAGIT_CMD} verifyvalid #{@args['input_path']}")
+  end
 end
-

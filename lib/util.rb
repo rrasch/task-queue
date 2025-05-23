5c6ac449da0008f7affc307e77607d36d3664e1f
Rasan Rasch <rasan@nyu.edu>
2025-05-23 09:28:29 -0400

    Refactor env to avoid rubocop warnings

--- lib/util.rb
+++ lib/util.rb
@@ -20,9 +20,8 @@ class Util
     @cmd.do_cmd('false')
   end
 
-  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-  def env
-    ruby_info = <<~INFO
+  def ruby_info
+    <<~INFO
       Ruby Version: #{RUBY_VERSION}
       Patch Level: #{RUBY_PATCHLEVEL}
       Platform: #{RUBY_PLATFORM}
@@ -32,37 +31,48 @@ class Util
       Executable: #{RbConfig.ruby}
       Current Directory: #{Dir.pwd}
       Load Path: #{$LOAD_PATH.join(':')}
-      Environment Variables Count: #{ENV.size}
+    INFO
+  end
 
+  def gem_info
+    <<~INFO
       Gem environment:
       Gem Sources: #{Gem.default_sources.join(':')}
       Gem Directory: #{Gem.dir}
       Gem Path: #{Gem.path.join(':')}
     INFO
+  end
 
+  def gem_info_from_cmd
     ruby_bin_dir = File.dirname(RbConfig.ruby)
     ruby_bin_name = File.basename(RbConfig.ruby)
     gem_bin_path = File.join(ruby_bin_dir, ruby_bin_name.sub('ruby', 'gem'))
-    gem_env_result = @cmd.do_cmd("#{gem_bin_path} env")
+    gem_env_cmd = "#{gem_bin_path} env"
+    gem_env_output = @cmd.do_cmd(gem_env_cmd)[:output]
+    "Output of '#{gem_env_cmd}':\n#{gem_env_output}"
+  end
 
-    installed_gems = Gem::Specification.sort_by(&:name).map do |spec|
+  def installed_gems
+    Gem::Specification.sort_by(&:name).map do |spec|
       "#{spec.name} (#{spec.version})"
     end.join("\n")
+  end
 
-    env_info = ENV.map { |key, value| "#{key}=#{value}" }.join("\n")
+  def env_info
+    ENV.map { |key, value| "#{key}=#{value}" }.join("\n")
+  end
 
+  def env
     full_info = <<~INFO
       #{ruby_info}
-      # Output of '#{gem_bin_path} env':
-      #{gem_env_result[:output]}
+      #{gem_info}
+      #{gem_info_from_cmd}
       Installed Gems:
       #{installed_gems}
 
       Environment Variables:
       #{env_info}
     INFO
-
     { success: true, output: full_info }
   end
-  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 end

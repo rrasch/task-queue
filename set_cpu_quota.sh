77a74aa12ce31d2cf646be30a07bbed1270e5c8e
Rasan Rasch <rasan@nyu.edu>
2026-08-26 05:31:12 -0400

    Check for rstar.slice

--- set_cpu_quota.sh
+++ set_cpu_quota.sh
@@ -2,9 +2,18 @@
 #
 # Set cpu quota for task queue to 75% of total cores,
 # but round down to a whole number of cores, with a
-# minimum of one core
+# minimum of one core. Exit without setting quota
+# if rstar.slice exists.
 
 set -eu
+
+SLICE=rstar.slice
+
+if systemctl list-units --all --type=slice --plain --no-legend "$SLICE" |
+    grep -q .; then
+    echo "Slice $SLICE exists."
+    exit
+fi
 
 NUM_CORES=$(nproc)
 TARGET_QUOTA=$((NUM_CORES * 75 / 100 * 100))
@@ -12,18 +21,10 @@ TARGET_QUOTA=$((NUM_CORES * 75 / 100 * 100))
 
 SERVICE=task-queue
 
-log() {
-    local msg="$*"
-    logger -t "$SERVICE" -p user.info -- "$msg"
-    if [ -t 1 ]; then
-        echo "$msg"
-    fi
-}
-
-log "Detected ${NUM_CORES} cores. Setting CPUQuota to ${TARGET_QUOTA}%."
+echo "Detected ${NUM_CORES} cores. Setting CPUQuota to ${TARGET_QUOTA}%."
 
 systemctl set-property --runtime "$SERVICE" CPUQuota="${TARGET_QUOTA}%"
 
 CURRENT_QUOTA=$(systemctl show "$SERVICE" -p CPUQuotaPerSecUSec)
 
-log "Systemd now reports $CURRENT_QUOTA"
+echo "Systemd now reports $CURRENT_QUOTA"

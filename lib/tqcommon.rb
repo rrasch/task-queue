41c509a800d3c99080ccdefda20e1e8d6a95336b
Rasan Rasch <rasan@nyu.edu>
2026-09-10 15:05:29 -0400

    Fix rubocop errors in tqcommon.rb

--- lib/tqcommon.rb
+++ lib/tqcommon.rb
@@ -1,57 +1,69 @@
+# frozen_string_literal: true
+
 require 'resolv'
 require 'socket'
 require 'yaml'
 
+# Provides common functionality for the task queue
 module TQCommon
+  INSTALL_DIR = '/usr/local/dlib/task-queue'
 
-  def self.get_env
-    env = /^d/ =~ Socket.gethostname ? "dev" : "prod"
-    return env
+  module_function
+
+  def env
+    /^d/ =~ Socket.gethostname ? 'dev' : 'prod'
   end
 
-  def self.get_sysconfig
-    conf_file = "/content/#{get_env()}/rstar/etc/task-queue.sysconfig"
+  def rstar_dir
+    "/content/#{env}/rstar"
+  end
+
+  def tmpdir
+    "#{rstar_dir}/tmp"
+  end
+
+  def sysconfig
+    conf_file = "/content/#{env}/rstar/etc/task-queue.sysconfig"
     config = {}
     File.foreach(conf_file) do |line|
       line.strip!
-      next if line.empty? || line.start_with?("#")
-      key, value = line.split("=", 2).map(&:strip)
+      next if line.empty? || line.start_with?('#')
+
+      key, value = line.split('=', 2).map(&:strip)
       config[key.downcase] = value
     end
-    return config
+    config
   end
 
-  def self.get_host_aliases
-    alias_file = "/content/#{get_env()}/rstar/etc/host-aliases.yaml"
+  def host_aliases
+    alias_file = "/content/#{env}/rstar/etc/host-aliases.yaml"
     aliases = {}
     if File.exist?(alias_file)
-      aliases = YAML.load_file(alias_file)["aliases"]
-      aliases = aliases.map { |name, nick| [name[/^[^.]+/], nick] }.to_h
+      aliases = YAML.load_file(alias_file)['aliases']
+      aliases = aliases.transform_keys { |name| name[/^[^.]+/] }
     end
-    return aliases
+    aliases
   end
 
-  def self.get_services
-    # services_file = "/content/#{get_env()}/rstar/etc/services.yaml"
-    services_file = File.expand_path("../services.yaml", File.dirname(__FILE__))
-    services = YAML.load_file(services_file)["services"]
-    return services
+  def services
+    # services_file = "/content/#{env}/rstar/etc/services.yaml"
+    services_file = File.expand_path('../services.yaml', File.dirname(__FILE__))
+    YAML.load_file(services_file)['services']
   end
 
-  def self.get_smtp_host
-    config = get_sysconfig()
-    return config.fetch("smtphost", "localhost")
+  def smtp_host
+    config = sysconfig
+    config.fetch('smtphost', 'localhost')
   end
 
-  def self.get_hostname(host)
+  def hostname(host)
     if /\A(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\Z/ =~ host
       begin
-        host = Resolv.getname(host)[/^[^.]+/].downcase
-      rescue Resolv::ResolvError => e
-        # Do nothing
+        host = Resolv.getname(host)[/^[^.]+/]&.downcase || host
+      rescue Resolv::ResolvError
+        nil
       end
     end
-    return host
+    host
   end
-
 end

f93cc6a9e3eb84e7c8d8c45d6bae3d8460256c10
Rasan Rasch <rasan@nyu.edu>
2026-09-25 03:35:32 -0400

    Set output path for bagit to the log file

--- lib/bagit.rb
+++ lib/bagit.rb
@@ -17,15 +17,40 @@ class Bagit
     @cmd = Cmd.new(@args, @logger)
   end
 
-  def validate
+  def validate # rubocop:disable Metrics/MethodLength
     unless @args['input_path']
       raise InvalidTaskError, 'Bagit.validate: Must specify input_path.'
     end
 
-    @cmd.do_cmd([BAGIT_CMD,
-                 'verifyvalid',
-                 @args['input_path'],
-                 '--noresultfile',
-                 *@args['extra_args'].shellsplit])
+    Dir.mktmpdir('bagit-') do |tmp_dir|
+      props_file = File.join(tmp_dir, 'log4j.properties')
+      write_log4j_properties(props_file, @args['output_path'])
+      @cmd['env'] =
+        { 'JAVA_OPTS' => "-Dlog4j.configuration=file:#{props_file}" }
+      @cmd.do_cmd([BAGIT_CMD,
+                   'verifyvalid',
+                   @args['input_path'],
+                   '--log-verbose',
+                   *@args['extra_args'].shellsplit])
+    end
+  end
+
+  private
+
+  def write_log4j_properties(properties_file, log_file)
+    File.write(properties_file, <<~PROPERTIES)
+      # Set root logger level to DEBUG and its appenders to CONSOLE and R.
+      log4j.rootLogger=ALL, R
+
+      # File
+      log4j.appender.R=org.apache.log4j.FileAppender
+      log4j.appender.R.File=#{log_file}
+      log4j.appender.R.layout=org.apache.log4j.PatternLayout
+      log4j.appender.R.layout.ConversionPattern=%d [%t] %-5p %C{1} : %m%n
+
+      # LIMIT CATEGORIES
+      log4j.logger.gov.loc.repository=INFO
+      log4j.logger.org.apache.http=INFO
+    PROPERTIES
   end
 end
